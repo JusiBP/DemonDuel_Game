82ce19e0994f4fcd93f0a9c767146216b446bed5
JB <josepbardesp@gmail.com>
2022-09-28 11:42:45 +0200

    commit pre cambios DOM DuelBlock

--- DemonDuel/JS/javascript.js
+++ DemonDuel/JS/javascript.js
@@ -9,6 +9,7 @@ class Demon {
       this.img30 = img30;
       this.ammo = 0;
       this.state = state;
+      this.duelOption = ""; //Option picked in duel (fire, load, shield)
     }
   }
 //Characters - objects (properties defined)
@@ -84,8 +85,6 @@ function activateCharacter(player, demon) {
       player2DemonObj = demon3;
     }
   }
-  //demon.health --> innerHTML in DUEL screen (prepare HTML)
-  //demon.ammo --> innerHTML in DUEL screen (prepare HTML)
 }
 
 //goDuel button in pick your character screen. --> takes us to DUEL screen.
@@ -115,9 +114,9 @@ duelScreenDiv.innerHTML = `<div class = "playerBox">
         <img id="imgDemonPlayer1" class="characterDuel" src="${player1DemonObj.img100}" alt="Player 1 Character">
     </div> 
     <div class="duelOptions">
-        <img class="duelButton" src="../Images/3_Fire.PNG" alt="Fire Button">
-        <img class="duelButton" src="../Images/3_Load.PNG" alt="Load Button">
-        <img class="duelButton" src="../Images/3_Shield.PNG" alt="Shield Button">
+        <img id="fireP1" class="duelButton" src="../Images/3_Fire.PNG" alt="Fire Button">
+        <img id="loadP1" class="duelButton" src="../Images/3_Load.PNG" alt="Load Button">
+        <img id="shieldP1" class="duelButton" src="../Images/3_Shield.PNG" alt="Shield Button">
     </div> 
 </div>
 <div class="log" > 
@@ -138,9 +137,9 @@ duelScreenDiv.innerHTML = `<div class = "playerBox">
         <img id="imgDemonPlayer2" class="characterDuel" src="${player2DemonObj.img100}" alt="Player 2 Character">
     </div> 
     <div class="duelOptions"> 
-        <img class="duelButton" src="../Images/3_Fire.PNG" alt="Fire Button">
-        <img class="duelButton" src="../Images/3_Load.PNG" alt="Load Button">
-        <img class="duelButton" src="../Images/3_Shield.PNG" alt="Shield Button">
+        <img id="fireP2" class="duelButton" src="../Images/3_Fire.PNG" alt="Fire Button">
+        <img id="loadP2" class="duelButton" src="../Images/3_Load.PNG" alt="Load Button">
+        <img id="shieldP2" class="duelButton" src="../Images/3_Shield.PNG" alt="Shield Button">
     </div> 
 </div>
 <div class = "log"> 
@@ -152,5 +151,158 @@ bodyforDom.appendChild(duelScreenDiv);
 }
 }
 
+//Duel Options & click to enable character picking
+let fireP1 = document.getElementById('fireP1');
+let loadP1 = document.getElementById('loadP1');
+let shieldP1 = document.getElementById('shieldP1');
+let fireP2 = document.getElementById('fireP2');
+let loadP2 = document.getElementById('loadP2');
+let shieldP2 = document.getElementById('shieldP2');
+
+fireP1.addEventListener("click", () => {activateActionP1(1,1)});
+fireP2.addEventListener("click", () => {activateActionP2(2,1)});
+loadP1.addEventListener("click", () => {activateActionP1(1,2)});
+loadP2.addEventListener("click", () => {activateActionP2(2,2)});
+shieldP1.addEventListener("click", () => {activateActionP1(1,3)});
+shieldP2.addEventListener("click", () => {activateActionP2(2,3)});
+
+function activateActionP1(player, option){
+  imgDemonPlayer1.classList.add("characterSelected") // Player picked an option to DUEL
+  if (player == 1 && option == 1){player1DemonObj.duelOption = "fire"}
+  else if (player == 1 && option == 2){player1DemonObj.duelOption = "load"}
+  else if (player == 1 && option == 3){player1DemonObj.duelOption = "shield"}
+}
+function activateActionP2(player, option){
+  imgDemonPlayer2.classList.add("characterSelected") // Player picked an option to DUEL
+  if (player == 2 && option == 1){player2DemonObj.duelOption = "fire"}
+  else if (player == 2 && option == 2){player2DemonObj.duelOption = "load"}
+  else if (player == 2 && option == 3){player2DemonObj.duelOption = "shield"}
+}
+
+if (player1DemonObj.duelOption.length > 0 && player2DemonObj.duelOption.length > 0){
+  duel()
+}
+
+//DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ---- DUEL ----
+//Both characters must choose a duel option
+function duel(){
+  // FIRE vs FIRE --------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "fire") && (player2DemonObj.duelOption = "fire")){
+    player1DemonObj.health -= player2DemonObj.attack;
+    player1DemonObj.ammo -= 1;
+    player2DemonObj.health -= player1DemonObj.attack;
+    player2DemonObj.ammo -= 1;
+    //P1 health update
+    if (player1DemonObj.health < (player1DemonObj.health * 0.6)){
+      imgDemonPlayer1.src = player1DemonObj.img60 //image state 2
+    }
+    else if (player1DemonObj.health < (player1DemonObj.health * 0.3)){
+      imgDemonPlayer1.src = player1DemonObj.img30 //image state 3
+    }
+    else if (player1DemonObj.health <= 0){
+      //WINNER PLAYER 2
+    }
+
+    //P2 health update
+    if (player2DemonObj.health < (player2DemonObj.health * 0.6)){
+      imgDemonPlayer2.src = player2DemonObj.img60 //image state 2
+    }
+    else if (player1DemonObj.health < (player1DemonObj.health * 0.3)){
+      imgDemonPlayer2.src = player2DemonObj.img30 //image state 3
+    }
+    else if (player2DemonObj.health <= 0){
+      //WINNER PLAYER 1
+    }
+  }
+
+  // FIRE vs SHIELD --------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "fire") && (player2DemonObj.duelOption = "shield")){
+    player1DemonObj.ammo -= 1;
+    player2DemonObj.health -= (player1DemonObj.attack * (player2DemonObj.shield / 100));
+
+    //P2 health update
+    if (player2DemonObj.health < (player2DemonObj.health * 0.6)){
+      imgDemonPlayer2.src = player2DemonObj.img60 //image state 2
+    }
+    else if (player2DemonObj.health < (player2DemonObj.health * 0.3)){
+      imgDemonPlayer2.src = player2DemonObj.img30 //image state 3
+    }
+    else if (player2DemonObj.health <= 0){
+      //WINNER PLAYER 1
+    }
+  }
+  if ((player2DemonObj.duelOption = "fire") && (player1DemonObj.duelOption = "shield")){
+    player2DemonObj.ammo -= 1;
+    player1DemonObj.health -= (player2DemonObj.attack * (player1DemonObj.shield / 100));
+
+    //P1 health update
+    if (player1DemonObj.health < (player1DemonObj.health * 0.6)){
+      imgDemonPlayer1.src = player1DemonObj.img60 //image state 2
+    }
+    else if (player1DemonObj.health < (player1DemonObj.health * 0.3)){
+      imgDemonPlayer1.src = player1DemonObj.img30 //image state 3
+    }
+    else if (player1DemonObj.health <= 0){
+      //WINNER PLAYER 2
+    }
+  }
+
+  // FIRE vs LOAD ---------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "fire") && (player2DemonObj.duelOption = "load")){
+    player1DemonObj.ammo -= 1;
+    player2DemonObj.ammo += 1;
+    player2DemonObj.health -= player1DemonObj.attack;
+
+    //P2 health update
+    if (player2DemonObj.health < (player2DemonObj.health * 0.6)){
+      imgDemonPlayer2.src = player2DemonObj.img60 //image state 2
+    }
+    else if (player2DemonObj.health < (player2DemonObj.health * 0.3)){
+      imgDemonPlayer2.src = player2DemonObj.img30 //image state 3
+    }
+    else if (player2DemonObj.health <= 0){
+      //WINNER PLAYER 1
+    }
+  }
+
+  if ((player1DemonObj.duelOption = "load") && (player2DemonObj.duelOption = "fire")){
+    player2DemonObj.ammo -= 1;
+    player1DemonObj.ammo += 1;
+    player1DemonObj.health -= player2DemonObj.attack;
+
+    //P1 health update
+    if (player1DemonObj.health < (player1DemonObj.health * 0.6)){
+      imgDemonPlayer1.src = player1DemonObj.img60 //image state 2
+    }
+    else if (player1DemonObj.health < (player1DemonObj.health * 0.3)){
+      imgDemonPlayer1.src = player1DemonObj.img30 //image state 3
+    }
+    else if (player1DemonObj.health <= 0){
+      //WINNER PLAYER 2
+    }
+  }
+
+  // SHIELD vs LOAD ----------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "shield") && (player2DemonObj.duelOption = "load")){
+    player2DemonObj.ammo += 1;
+  }
+  if ((player2DemonObj.duelOption = "shield") && (player1DemonObj.duelOption = "load")){
+    player1DemonObj.ammo += 1;
+  }
+
+  // SHIELD vs SHIELD --------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "shield") && (player2DemonObj.duelOption = "shield")){}
+
+  // LOAD vs LOAD ------------------------------------------------------------------------
+  if ((player1DemonObj.duelOption = "load") && (player2DemonObj.duelOption = "load")){
+    player1DemonObj.ammo += 1;
+    player2DemonObj.ammo += 1;
+  }
+  //--------------------------------------------------------------------------------------
+  player1DemonObj.duelOption = "";
+  player2DemonObj.duelOption = "";
+  imgDemonPlayer1.classList.remove("characterSelected");
+  imgDemonPlayer2.classList.remove("characterSelected");
+}
 
 
